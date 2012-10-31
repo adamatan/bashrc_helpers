d80d9ef8f0b5a06988498b8008c23f97d0291f3d
Adam Matan <adam@matan.name>
2012-10-31 15:11:23 +0200

    Add rsync alias

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -3,5 +3,5 @@
 # Copyright (c) 2012 Adam Matan <adam@matan.name>
 # See the file license.txt for copying permission.
 
-alias gerp='grep'  		# Helps with Dislexya
-
+alias gerp='grep'  		     # Helps with Dislexya
+alias rsyn='rsync -avzr --progress'  # I want this for every remote sync
